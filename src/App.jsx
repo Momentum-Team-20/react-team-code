e16078148ddf745e934f7586c960d543e666472f
Jordan Manus <jlmanus17@gmail.com>
2023-12-07 11:23:34 -0500

    added card function to app that will flip card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react"
+
 function App() {
   const emojis = ["🌸", "⚡️", "👻", "👽", "🐬", "🍄"]
   const back = "🍕"
+
   return (
     <main>
       <div className="cardContainer">
         {emojis.map((emoji) => (
-          {emoji}
+          // <div>{emoji}</div>
+          <Card
+            front_card={emoji}
+            back_card={back}
+          />
         ))}
       </div>
     </main>
   )
 }
 
+function Card({ front_card, back_card }) {
+  // finish writing useState for flipping card over
+  // const []
+
+  return (
+    <div>
+      <div>{front_card}</div>
+      <div>{back_card}</div>
+    </div>
+  )
+}
+
+
 export default App
+
